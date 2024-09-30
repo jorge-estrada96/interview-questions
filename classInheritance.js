b9953544ca83398fcd30382860b54e2e04a0f2b3
jorge-estrada96 <jorge@reverscore.com>
2024-09-30 12:54:51 -0600

    solved typos

--- classInheritance.js
+++ classInheritance.js
@@ -1,32 +1,31 @@
 class Animal {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
-  eat(){
-    console.log('Eating')
+  eat() {
+    console.log("Eating");
   }
 }
 
 class Dog extends Animal {
   constructor(name, weight, age) {
-    super(name)
-    this.weight = weight
-    this.age = age
+    super(name);
+    this.weight = weight;
+    this.age = age;
   }
-  bark(){
-    console.log(`${this.name} is barking`)
+  bark() {
+    console.log(`${this.name} is barking`);
   }
 }
 
 Dog.prototype.jump = function () {
-  console.log(`${this.name} is jumping`)
-}
+  console.log(`${this.name} is jumping`);
+};
 
+const gatsby = new Dog("gatsby", 12, 1);
+gatsby.eat();
+gatsby.bark();
+gatsby.jump();
 
-const gastby = new Dog('gatsby', 12, 1)
-gastby.eat()
-gastby.bark()
-gastby.jump()
-
-const ringo = new Dog('ringo', 10, 12)
-ringo.jump()
+const ringo = new Dog("ringo", 10, 12);
+ringo.jump();
